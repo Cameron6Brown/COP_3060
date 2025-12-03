555b6061501a0f5c5860660ab1e1b0f954054e5d
Cameron6Brown <cameron6brown@gmail.com>
2025-12-02 17:06:31 -0800

    Create TMDBSearchResponseDTO for API response
    Added TMDBSearchResponseDTO class for TMDB Search API response.

--- dto/external/TMDBSearchResponseDTO.java
+++ dto/external/TMDBSearchResponseDTO.java
@@ -1,1 +1,18 @@
+package com.cameron.cop3060.dto.external;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
+
+/**
+ * DTO for TMDB Search API response wrapper
+ * 
+ * @author Cameron Brown
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TMDBSearchResponseDTO {
+
+    private Integer page;
+
+    @JsonProperty("total_pages")
+    private Integer totalPages;
