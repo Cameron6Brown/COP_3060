f2843df047044a3a835796ce13c5a32bfe5011c1
Cameron6Brown <cameron6brown@gmail.com>
2025-12-02 17:06:19 -0800

    Add TMDBMovieDTO for TMDB Movie API response
    This class serves as a Data Transfer Object (DTO) for the TMDB Movie API response, encapsulating various movie attributes.

--- dto/external/TMDBMovieDTO.java
+++ dto/external/TMDBMovieDTO.java
@@ -1,1 +1,117 @@
+package com.cameron.cop3060.dto.external;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
+
+/**
+ * DTO for TMDB Movie API response
+ * 
+ * @author Cameron Brown
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TMDBMovieDTO {
+
+    private Long id;
+    private String title;
+
+    @JsonProperty("original_title")
+    private String originalTitle;
+
+    private String overview;
+
+    @JsonProperty("poster_path")
+    private String posterPath;
+
+    @JsonProperty("backdrop_path")
+    private String backdropPath;
+
+    @JsonProperty("release_date")
+    private String releaseDate;
+
+    @JsonProperty("vote_average")
+    private Double voteAverage;
+
+    @JsonProperty("vote_count")
+    private Integer voteCount;
+
+    private Double popularity;
+
+    @JsonProperty("genre_ids")
+    private List<Integer> genreIds;
+
+    @JsonProperty("original_language")
+    private String originalLanguage;
+
+    private Boolean adult;
+    private Boolean video;
+
+    // Runtime (only available in detailed response)
+    private Integer runtime;
+
+    // Constructors
+    public TMDBMovieDTO() {}
+
+    // Getters and Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getOriginalTitle() { return originalTitle; }
+    public void setOriginalTitle(String originalTitle) { this.originalTitle = originalTitle; }
+
+    public String getOverview() { return overview; }
+    public void setOverview(String overview) { this.overview = overview; }
+
+    public String getPosterPath() { return posterPath; }
+    public void setPosterPath(String posterPath) { this.posterPath = posterPath; }
+
+    public String getBackdropPath() { return backdropPath; }
+    public void setBackdropPath(String backdropPath) { this.backdropPath = backdropPath; }
+
+    public String getReleaseDate() { return releaseDate; }
+    public void setReleaseDate(String releaseDate) { this.releaseDate = releaseDate; }
+
+    public Double getVoteAverage() { return voteAverage; }
+    public void setVoteAverage(Double voteAverage) { this.voteAverage = voteAverage; }
+
+    public Integer getVoteCount() { return voteCount; }
+    public void setVoteCount(Integer voteCount) { this.voteCount = voteCount; }
+
+    public Double getPopularity() { return popularity; }
+    public void setPopularity(Double popularity) { this.popularity = popularity; }
+
+    public List<Integer> getGenreIds() { return genreIds; }
+    public void setGenreIds(List<Integer> genreIds) { this.genreIds = genreIds; }
+
+    public String getOriginalLanguage() { return originalLanguage; }
+    public void setOriginalLanguage(String originalLanguage) { this.originalLanguage = originalLanguage; }
+
+    public Boolean getAdult() { return adult; }
+    public void setAdult(Boolean adult) { this.adult = adult; }
+
+    public Boolean getVideo() { return video; }
+    public void setVideo(Boolean video) { this.video = video; }
+
+    public Integer getRuntime() { return runtime; }
+    public void setRuntime(Integer runtime) { this.runtime = runtime; }
+
+    // Helper method to get full poster URL
+    public String getFullPosterUrl(String baseUrl) {
+        return posterPath != null ? baseUrl + posterPath : null;
+    }
+
+    // Helper method to extract year from release date
+    public Integer getReleaseYear() {
+        if (releaseDate != null && releaseDate.length() >= 4) {
+            try {
+                return Integer.parseInt(releaseDate.substring(0, 4));
+            } catch (NumberFormatException e) {
+                return null;
+            }
+        }
+        return null;
+    }
+}
